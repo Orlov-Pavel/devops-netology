75f22a87393254f9351d79c4ce70b85ff46d0a92
Pavel <kive2012@mail.ru>
2022-06-21 22:41:35 +0300

    Merge branch 'git-merge'

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,11 +3,7 @@
 
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
-    echo "\$@ Parameter #$count = $param"
-=======
-    echo "Parameter: $param"
->>>>>>> 18e82aa (git-rebase 1)
+    echo "Next parameter: $param"
     count=$(( $count + 1 ))
 done
 
